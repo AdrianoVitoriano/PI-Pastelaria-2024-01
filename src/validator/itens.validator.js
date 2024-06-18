7e096003b5e412771856899bbd836369bcf1b865
Adriano V. da Silva <adrianovitorianosilva@gmail.com>
2024-06-18 12:03:22 -0300

    retirando campo inexistente do validato

--- src/validator/itens.validator.js
+++ src/validator/itens.validator.js
@@ -16,7 +16,6 @@ export const createItensValidator = [
     body('nome').isString().withMessage("Nome não foi passado ou não é um texto."),
     body('preco').isFloat().withMessage("Preço não foi passado ou não é um número"),
     body('idTipo').isInt().withMessage("idTipo não foi passado ou não é um número inteiro"),
-    body('tipo').isInt().withMessage("Tipo não foi passado ou não é um número inteiro"),
 ]
 
 export const updateItensValidator = [
@@ -35,5 +34,5 @@ export const updateItensValidator = [
     body('nome').optional().isString().withMessage("Nome não é um texto."),
     body('preco').optional().isFloat().withMessage("Preco não é um número"),
     body('idTipo').optional().isInt().withMessage("idTipo não é um número inteiro"),
-    body('tipo').isInt().withMessage("Tipo não foi passado ou não é um número inteiro"),
+
 ]
